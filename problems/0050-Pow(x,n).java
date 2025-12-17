fd8e623fac13b38d899c5e221d36bb19e538676d
Scarlett <98492102+sijia1120@users.noreply.github.com>
2025-12-17 21:20:26 +0000

    Update 0050-Pow(x,n).java

--- problems/0050-Pow(x,n).java
+++ problems/0050-Pow(x,n).java
@@ -1,1 +1,28 @@
+// ===   Questions   ===:
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
+// ===   Solutions ===:
+
+class Solution {
+    public double myPow(double x, int n) {
+        
+        // Any number to the power of 0 equals 1. Since the problem requires returning double type, return 1.0
+        if (n== 0) return 1.0;
+
+        // Case 1: If n is even
+        if ((n & 1)==0){
+            return myPow(x * x, n / 2);
+
+        // Case 2: If n is odd
+        } else {
+
+            // Case 2.1: n is positive
+            if (n>0) return myPow(x*x,n/2) * x;
+            if (n < 0) return myPow(x*x, n/2) * 1/x;
+
+            // Case 2.2: n is negative
+        }
+        return -1;
+    }
+
+}
